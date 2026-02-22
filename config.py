7568a900f9b351f766ca6a6a3e4b9f0d81d569da
ANI_OTAKU <dhirenyadav2007@gmail.com>
2026-02-22 16:26:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ MSG_EFFECT = 5046509860389126442
 
 SHORT_URL = "shrinkme.io" # shortner url 
 SHORT_API = "e75f29485ddb045e6887d284f762f3daeb84f869" 
-SHORT_TUT = "https://t.me/How_to_Download_7x/26"
+SHORT_TUT = "https://t.me/ANIME_X_FLEX/19"
 
 # Bot Configuration
 SESSION = "ALYA"
